983887229b3a6dcfc4dc746e31ad047a3e4ca07e
ChrisDChar <zsultanova425@gmail.com>
2025-12-01 02:15:09 +0500

    load

--- js/single.js
+++ js/single.js
@@ -50,7 +50,6 @@ function goBackToList() {
 
 async function loadSingleContent(type, id) {
     const detailContent = document.getElementById('detailContent');
-    showLoader(detailContent, `Loading ${type === 'teacher' ? 'Teacher' : 'Student'}`);
     detailContent.innerHTML = `
         <div class="flex justify-center items-center py-12">
             <div class="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500"></div>
